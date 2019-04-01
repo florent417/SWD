baee13be3c444a79cf31e0e958eca3e9aec1edf1
florent417 <flole@live.dk>
2019-04-01 20:16:32 +0200

    W10 threads ex 1-8 done

---
+++ W10_1_Threads/Threading/Threading/HelloWriter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    class HelloWriter
+    {
+        public string Name { get; set; }
+        public int NumberOfIteratons { get; set; }
+        public int SleepTime { get; set; }
+
+        public HelloWriter()
+        {
+        }
+
+        public HelloWriter(int nrOfIterations)
+        {
+            NumberOfIteratons = nrOfIterations;
+        }
+
+        public void SayHello(object parameter)
+        {
+            int numberOfIterations = (int) parameter;
+            for (int i = 0; i < numberOfIterations; i++)
+            {
+                Console.WriteLine($"Hello from {Name} #{i}");      
+                Thread.Sleep(SleepTime);
+            }
+        }
+    }
+}
---
+++ W10_1_Threads/Threading/Threading/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWriter writer1 = new HelloWriter()
+            {
+                Name="Flow",
+                SleepTime = 200
+                //NumberOfIteratons = 50
+            };
+            HelloWriter writer2 = new HelloWriter()
+            {
+                Name = "Test",
+                SleepTime = 500
+                //NumberOfIteratons = 10
+            };
+
+            Thread writer1_t = new Thread(() => writer1.SayHello(20));
+            Thread writer2_t = new Thread(() => writer2.SayHello(20));
+            Thread neverending_t = new Thread(NeverEndingStoryThread);
+
+            writer1_t.Start();
+            writer2_t.Start();
+            neverending_t.Start();
+
+            writer1_t.Join();
+            writer2_t.Join();
+            ShallStop = true;
+            Console.WriteLine("Hello from main");
+        }
+
+        static void NeverEndingStoryThread()
+        {
+            while (!ShallStop)
+            {
+                Console.WriteLine("Never ending story");
+                Thread.Sleep(5000);
+            }
+        }
+
+        static bool ShallStop;
+    }
+}
