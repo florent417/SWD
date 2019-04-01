9458d222d339e618a362860ad3cdc58cd68126f5
florent417 <flole@live.dk>
2019-04-01 19:22:48 +0200

    Moved L5 files into a folder

---
+++ L5_01_Template_Strategy_Method/L05_01_Template_And_Strategy_method/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L05_01_Template_And_Strategy_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayGenerator testArray = new ArrayGenerator(10,60,8);
+
+            foreach (var element in testArray.RandomNumberArray)
+            {
+                Console.WriteLine("{0}",element);
+            }
+        }
+    }
+}
---
+++ L5_01_Template_Strategy_Method/L05_01_Template_And_Strategy_method/Sorters/ISort.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L05_01_Template_And_Strategy_method
+{
+    interface ISort
+    {
+        int[] SortArray(int[] unsortedArray);
+    }
+}
